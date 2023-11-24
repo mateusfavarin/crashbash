d8f9edda929d3fb0ad40521ca36fe94fa281f32a
mateusfavarin <mateusfavarinc@gmail.com>
2023-11-24 15:24:06 -0300

    Fix printf when sp is in scratchpad

--- decomp/core/kernel.s
+++ decomp/core/kernel.s
@@ -7,3 +7,22 @@ printf:
     li    $t2, 0xA0
     jr    $t2
     li    $t1, 0x3F
+
+.set noreorder
+.align 2
+.global getStackPtr
+.type getStackPtr, @function
+
+getStackPtr:
+    jr    $ra
+    move  $v0, $sp
+
+.set noreorder
+.align 2
+.global setStackPtr
+.type setStackPtr, @function
+
+setStackPtr:
+    move  $v0, $sp
+    jr    $ra
+    move  $sp, $a0
--- include/kernel.h
+++ include/kernel.h
@@ -7,10 +7,17 @@
 #if DEBUG == 0
     #define LOG(...)
 #else
-    #define LOG(...) printf(__VA_ARGS__)
+    // we need to make sure that our stack has enough space for printf
+    #define LOG(...) \
+        void * __pCurrStack__ = setStackPtr((void *) 0x80600000); \
+        printf(__VA_ARGS__); \
+        setStackPtr(__pCurrStack__)
+
 #endif
 
-int printf(char * format, ... );
+s32 printf(char * format, ... );
+void * getStackPtr();
+void * setStackPtr(void * p);
 
 force_inline void pcsx_putc(int c) {
     *((volatile char * const) 0x1f802080) = c;
